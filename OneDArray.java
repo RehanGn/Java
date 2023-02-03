c5a2d415114fe17d2ea73b48d9dc2c0c426b6d63
RehanGn <94187098+RehanGn@users.noreply.github.com>
2023-02-03 22:44:12 +0530

    Add files via upload
    Array

---
+++ OneDArray.java
@@ -0,0 +1,11 @@
+public class OneDArray {
+    public static void main(String[] args){
+        //1D array
+        int a[] = {3,46,35,36,56,467,66,464,354};
+     //   System.out.println(a[1]);
+        for (int b : a){
+            System.out.print(b+"  ");
+        }
+    }
+
+}
---
+++ inputasaclient.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class inputasaclient {
+    public static void main(String[] args){
+        int a[] = new int[5];
+        System.out.println("Enter your number: ");
+        Scanner sc = new Scanner(System.in);
+        for (int b:a){
+            System.out.println(b);
+        }
+    }
+}
